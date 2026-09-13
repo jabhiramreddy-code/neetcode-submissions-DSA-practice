edcfbb5c07eaf905fc0a9a89f28c2393f82d103f
jabhiramreddy-code <jabhiramreddy.work@gmail.com>
2026-09-13 22:01:58 +0530

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    encoder = ";asldkjf439i5"
+    def encode(self, strs: List[str]) -> str:
+        encoder = ";asldkjf439i5"
+        # ans=""
+        # for i in strs:
+        #     ans += i + encoder
+        # return ans
+        return (encoder).join(strs)+"..."+str(len(strs))
+
+    def decode(self, s: str) -> List[str]:
+        encoder = ";asldkjf439i5"
+        res = s.split("...")
+        if res[1] != '0':
+            return res[0].split(encoder)
+        return []
